777252517df167083798673632284a5b1d1b2d7f
Gonzalo Sola <gonzalosola@gmail.com>
2019-10-11 01:15:49 -0300

    Clases basicas

---
+++ src/main/java/Aplicacion.java
@@ -0,0 +1,51 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class Aplicacion implements WindowListener, ActionListener {
+
+    public static void main(String[] args){
+        InterfazGrafica ig = new InterfazGrafica();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+
+    @Override
+    public void windowOpened(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowClosed(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent windowEvent) {
+
+    }
+}
---
+++ src/main/java/InterfazGrafica.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class InterfazGrafica {
+
+    JFrame frame;
+
+
+    public InterfazGrafica(){
+        frame = new JFrame("");
+        
+    }
+
+}
