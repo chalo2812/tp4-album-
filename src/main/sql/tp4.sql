ed7c01c130e1062c4a7a679ef240376c949a3e38
chalo2812 <gonzalosola@gmail.com>
2019-11-02 02:36:18 -0300

    Falta la columna NRO_ARTISTA en TEMA y lo agregue en los insert

--- src/main/sql/tp4.sql
+++ src/main/sql/tp4.sql
@@ -22,7 +22,7 @@ CREATE DATABASE `tp4`;
 --
 -- Estructura de tabla para la tabla `artista`
 --
-CREATE TABLE `artista` (
+CREATE TABLE `tp4`.`artista` (
   `NRO_ARTISTA` int(11) NOT NULL,
   `NOMBREARTISTA` varchar(100) NOT NULL,
   `NACIONALIDAD` varchar(30) NOT NULL,
@@ -31,12 +31,12 @@ CREATE TABLE `artista` (
 --
 -- Volcado de datos para la tabla `artista`
 --
-INSERT INTO `artista` (`NRO_ARTISTA`, `NOMBREARTISTA`, `NACIONALIDAD`, `GENERO_MUSICAL`) VALUES
+INSERT INTO `tp4`.`artista` (`NRO_ARTISTA`, `NOMBREARTISTA`, `NACIONALIDAD`, `GENERO_MUSICAL`) VALUES
 (1, 'DIEGO TORRES', 'ARGENTINA', 'ROCK NACIONAL'),
 (2, 'SHAKIRA', 'COLOMBIANA', 'POP');
 
 
-CREATE TABLE `album` (
+CREATE TABLE `tp4`.`album` (
   `NRO_ALBUM` int(11) NOT NULL,
   `NRO_ARTISTA` int(11) NOT NULL,
   `FECHA_PUBLICACION` varchar(20) NOT NULL,
@@ -48,38 +48,38 @@ CREATE TABLE `album` (
 -- Volcado de datos para la tabla `album`
 --
 
-INSERT INTO `album` (`NRO_ALBUM`, `NRO_ARTISTA`, `FECHA_PUBLICACION`, `DISCOGRAFICA`, `NOMBRE_ALBUM`) VALUES
+INSERT INTO `tp4`.`album` (`NRO_ALBUM`, `NRO_ARTISTA`, `FECHA_PUBLICACION`, `DISCOGRAFICA`, `NOMBRE_ALBUM`) VALUES
 (1, 2, '06/10/1995', 'EPIC RECORD', 'PIES DESCALZOS'),
 (2, 0, '10/07/1997', 'RCA RECORD', 'TRATAR DE ESTAR MEJOR');
-
 
 -- --------------------------------------------------------
 --
 -- Estructura de tabla para la tabla `tema`
 --
-CREATE TABLE `tema` (
+CREATE TABLE `tp4`.`tema` (
   `NRO_TEMA` int(11) NOT NULL,
   `NRO_ALBUM` int(11) NOT NULL,
+  `NRO_ARTISTA` int(11) NOT NULL,
   `DESCRIPCION` varchar(30) NOT NULL,
   `DURACION` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-INSERT INTO `tema` (`NRO_TEMA`, `NRO_ALBUM`, `DESCRIPCION`, `DURACION`) VALUES 
-('1', '1','Estoy aquí','5'),
-('2', '1','¿Dónde estas, corazón?','5'),
-('3', '1','Pies descalzos, sueños blancos','5'),
-('4', '1','Un poco de amor','5'),
-('5', '1','Antología','5'),
-('6','','Se quiere, se mata','5'),
-('1','2','Tratar de estar mejor', '3'),
-('2','2','Todo cambia (Y todo se termina)','5'),
-('3','2','Dame una razón','4'),
-('4','2','Aunque quieras','4'),
-('5','2','Deja de pedir perdón', 	'3'),
-('6','2','Te pido que vuelvas', 	'4'),
-('7','2','Pensar (Que siempre hay alguien más)', 	'4'),
-('8','2','San Salvador', 	'4'),
-('9','2','Secretos del mar'	,'3'),
-('10','2','Por la vereda del sol (Walking On Sunshine)',	'4');
+INSERT INTO `tp4`.`tema` (`NRO_TEMA`, `NRO_ALBUM`, `NRO_ARTISTA`,`DESCRIPCION`, `DURACION`) VALUES 
+('1', '1','2','Estoy aquí','5'),
+('2', '1','2','¿Dónde estas, corazón?','5'),
+('3', '1','2','Pies descalzos, sueños blancos','5'),
+('4', '1','2','Un poco de amor','5'),
+('5', '1','2','Antología','5'),
+('6','1','2','Se quiere, se mata','5'),
+('1','2','1','Tratar de estar mejor', '3'),
+('2','2','1','Todo cambia (Y todo se termina)','5'),
+('3','2','1','Dame una razón','4'),
+('4','2','1','Aunque quieras','4'),
+('5','2','1','Deja de pedir perdón', 	'3'),
+('6','2','1','Te pido que vuelvas', 	'4'),
+('7','2','1','Pensar (Que siempre hay alguien más)', 	'4'),
+('8','2','1','San Salvador', 	'4'),
+('9','2','1','Secretos del mar'	,'3'),
+('10','2','1','Por la vereda del sol (Walking On Sunshine)',	'4');
 
 --
 -- Índices para tablas volcadas
@@ -88,7 +88,7 @@ INSERT INTO `tema` (`NRO_TEMA`, `NRO_ALBUM`, `DESCRIPCION`, `DURACION`) VALUES
 --
 -- Indices de la tabla `artista`
 --
-ALTER TABLE `artista`
+ALTER TABLE `tp4`.`artista`
   ADD KEY `NRO_ARTISTA` (`NRO_ARTISTA`);
 COMMIT;
 
